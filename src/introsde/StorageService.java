9bb2ceeec7d6647cf51dfc8552c90e77f36c7b06
Kristian Segnana <kristian.segnana@gmail.com>
2016-03-23 13:15:23 +0100

    fix all!!

--- src/introsde/StorageService.java
+++ src/introsde/StorageService.java
@@ -17,7 +17,7 @@ import javax.xml.ws.WebServiceFeature;
  * Generated source version: 2.2
  * 
  */
-@WebServiceClient(name = "StorageService", targetNamespace = "http://soap.sde/", wsdlLocation = "http://immense-shore-7749.herokuapp.com/ws/storage?wsdl")
+@WebServiceClient(name = "StorageService", targetNamespace = "http://soap.sde/", wsdlLocation = "http://sheltered-stream-88810.herokuapp.com/ws/storage?wsdl")
 public class StorageService
     extends Service
 {
@@ -30,7 +30,7 @@ public class StorageService
         URL url = null;
         WebServiceException e = null;
         try {
-            url = new URL("http://immense-shore-7749.herokuapp.com/ws/storage?wsdl");
+            url = new URL("http://sheltered-stream-88810.herokuapp.com/ws/storage?wsdl");
         } catch (MalformedURLException ex) {
             e = new WebServiceException(ex);
         }
